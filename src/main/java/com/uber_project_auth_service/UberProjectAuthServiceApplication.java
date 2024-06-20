89b0ebe75c3e0bb58ad350aeda213ec41a8a5c1e
venkateshkarri <venkatesh1999vlb@gmail.com>
2024-06-20 15:08:45 +0530

    First commit

---
+++ src/main/java/com/uber_project_auth_service/UberProjectAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.uber_project_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberProjectAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberProjectAuthServiceApplication.class, args);
+	}
+
+}
