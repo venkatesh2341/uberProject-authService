3a3e48729af91025e9324f670fdc378882752c82
venkateshkarri <venkatesh1999vlb@gmail.com>
2024-07-03 17:06:24 +0530

    Added to get the new changes from entity provider

--- src/main/java/com/uber_project_auth_service/Repositories/DriverRepository.java
+++ src/main/java/com/uber_project_auth_service/Repositories/DriverRepository.java
@@ -1,16 +1,13 @@
 package com.uber_project_auth_service.Repositories;
 
-
 import com.uber_project.entity_provider.Models.Driver;
-import com.uber_project.entity_provider.Models.Passenger;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface AuthRepository extends JpaRepository<Passenger, Long> {
+public interface DriverRepository extends JpaRepository<Driver,Long> {
 
-    Optional<Passenger> findPassengerByEmail(String email);
-
+    Optional<Driver> findDriverByAadhaar(String aadhaar);
 }
